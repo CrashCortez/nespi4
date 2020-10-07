fa979d44c97b45eeffc5d7d0d735c35037934964
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2020-10-06 20:46:56 -0700

    Create eeprom_update.sh

---
+++ eeprom_update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo sed 's/critical/stable/'
+/etc/default/rpi-eeprom-update
+sudo apt update
+sudo apt -y full-upgrade
+sudo rpi-eeprom-update -a
+sudo reboot
+
+#after reboot run ‘sudo rpi-eeprom-update’ you should have a eeprom current of 09 03 2020 or greater. You need need at least 07 16 2020 to boot from usb/HDD/ssd etc.
